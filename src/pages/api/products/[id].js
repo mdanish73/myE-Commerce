bd42a1e5d0644d6dfde8a550025d20e1ea9f969e
mdanish73 <mdiqbal00037@gmail.com>
2023-11-05 15:59:24 +0500

    api done

--- src/pages/api/products/[id].js
+++ src/pages/api/products/[id].js
@@ -26,6 +26,7 @@ export default async function handler(req, res){
     try {
         switch(req.method){
             case 'GET':
+                await ProductsModel.findById(req.query.id);
                 res.status(200).json({
                     success: true,
                     message: foundProduct
